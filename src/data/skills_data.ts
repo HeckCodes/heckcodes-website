1e9607871514e18bc329167ff6f2cc12998dac67
Heck Codes <chandramaulishastri@gmail.com>
2023-11-26 16:54:52 +0530

    Added skills section

---
+++ src/data/skills_data.ts
@@ -0,0 +1,37 @@
+type Skill = {
+    label: string;
+    imgPath: string;
+}
+
+const skills: Skill[] = [
+    {
+        label: "C/C++",
+        imgPath: "c.png"
+    },
+    {
+        label: "Java",
+        imgPath: "java.png"
+    },
+    {
+        label: "Python",
+        imgPath: "python.png"
+    },
+    {
+        label: "Javascript",
+        imgPath: "javascript.png"
+    },
+    {
+        label: "Flutter",
+        imgPath: "flutter.png"
+    },
+    {
+        label: "React",
+        imgPath: "react.png"
+    },
+    {
+        label: "Firebase",
+        imgPath: "firebase.png"
+    },
+]
+
+export default skills;
